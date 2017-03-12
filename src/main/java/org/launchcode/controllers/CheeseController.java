c8ab7e3f9383ccc0e810bb97f1262e6a9a5f8252
Debbie Eidelman <jazdes13@charter.net>
2017-03-11 20:10:20 -0600

    cheese classwork description add

--- src/main/java/org/launchcode/controllers/CheeseController.java
+++ src/main/java/org/launchcode/controllers/CheeseController.java
@@ -2,6 +2,7 @@ package org.launchcode.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -38,6 +39,24 @@ public class CheeseController {
         cheeses.put(cheeseName, description);
         // Redirect to /cheese
         return "redirect:";
+    }
+
+
+    @RequestMapping(value = "delete", method = RequestMethod.GET)
+    public String displayDeleteForm (Model model) {
+        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("title", "My Cheeses");
+
+        return "cheese/delete";
+    }
+
+    // cheese/deletecheese
+
+    @RequestMapping(value= "deletecheese", method = RequestMethod.GET)
+    public String deleteCheese(@RequestParam("name") String cheeseToBeDeleted) {
+        cheeses.remove(cheeseToBeDeleted);
+        return "redirect:";
+
     }
 
 
